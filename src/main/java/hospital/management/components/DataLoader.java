5b2c1c05988b65f93e27772f10df91058266e069
Kacper <kapelek82@outlook.com>
2023-12-20 18:43:24 +0000

    Begun setting up DataLoader and DepartamentController

--- src/main/java/hospital/management/components/DataLoader.java
+++ src/main/java/hospital/management/components/DataLoader.java
@@ -1,4 +1,34 @@
 package hospital.management.components;
 
-public class DataLoader {
+import hospital.management.models.Hospital;
+import hospital.management.repositories.DepartmentRepository;
+import hospital.management.repositories.DoctorRepository;
+import hospital.management.repositories.HospitalRepository;
+import hospital.management.repositories.NurseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+    @Autowired
+    DoctorRepository doctorRepository;
+    @Autowired
+    HospitalRepository hospitalRepository;
+    @Autowired
+    NurseRepository nurseRepository;
+
+
+    public DataLoader(){
+
+    }
+    @Override
+    public void run(ApplicationArguments args) throws Exception{
+        Hospital stJohns = new Hospital("StJohns", "London SE25");
+        hospitalRepository.save(stJohns);
+    }
 }
--- src/main/java/hospital/management/controllers/DepartmentController.java
+++ src/main/java/hospital/management/controllers/DepartmentController.java
@@ -1,4 +1,17 @@
 package hospital.management.controllers;
 
+import hospital.management.repositories.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class DepartmentController {
+
+//    @Autowired
+//    DepartmentRepository departmentRepository;
+//
+//    @GetMapping
+//    public ResponseEntity<List<>> getAllDepartaments(){
+//
+//    }
 }
