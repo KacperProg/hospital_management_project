a4e073f143804be920608f1b6b3531bccf85f2a6
Kacper <kapelek82@outlook.com>
2023-12-20 20:09:59 +0000

    Expanded on DataLoader information

--- src/main/java/hospital/management/controllers/DoctorController.java
+++ src/main/java/hospital/management/controllers/DoctorController.java
@@ -1,4 +1,9 @@
 package hospital.management.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/doctors")
 public class DoctorController {
 }
