77a0ee385c20f5687e7bf15a9036ffcdbe78a078
Janet_Mensah <janetmensah60@gmail.com>
2023-12-21 16:42:50 +0000

    Created method for retrieving nurse by Id in service and controller

--- src/main/java/hospital/management/services/NurseService.java
+++ src/main/java/hospital/management/services/NurseService.java
@@ -1,4 +1,19 @@
 package hospital.management.services;
 
+import hospital.management.models.Nurse;
+import hospital.management.repositories.NurseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class NurseService {
+
+    @Autowired
+    NurseRepository nurseRepository;
+
+    public Optional<Nurse> findNurseById(Long id) {
+        return nurseRepository.findById(id);
+    }
 }
