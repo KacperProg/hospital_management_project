a637f99193c97707a1cf303894b0020d993d056b
Rayster Ramos <rjrfrst@gmail.com>
2023-12-21 17:45:19 +0000

    added function to add all doctors

--- src/main/java/hospital/management/services/DoctorService.java
+++ src/main/java/hospital/management/services/DoctorService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,11 @@ public class DoctorService {
     //Get doctor by id
     public Optional<Doctor> getDoctorById(Long id) {
         return doctorRepository.findById(id);
+    }
+
+    //Get all doctors
+    public List<Doctor> getAllDoctors(){
+        return doctorRepository.findAll();
     }
 
 
